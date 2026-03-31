b605d769cb052a97b1cccbd046d9e3716c624307
Eve <evenlybigman@gmail.com>
2026-03-31 15:52:02 +0900

    객체 배열

--- week05_array/array.cpp
+++ week05_array/array.cpp
@@ -19,5 +19,17 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cout << Circles[i].getArea() << "\t";
 	}
+	int n = 0;
+	int* q = &n;
+	q++;
+
+	cout << endl;
+	Circle* p;
+	p = Circles; // &Circles[0]과 같은 뜻, 주소는 정수 = p에는 정수값이 들어있음.
+	for (int i = 0; i < 3; i++) {
+		cout << p->getArea() << "\t";
+		p++; //p++ 한번지는 1바이트 int는 4바이트를 저장하기 때문에 4가 오름
+		//한바이트는 포켓몬인데
+	}
 	return 0;
 }
