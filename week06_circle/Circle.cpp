3cefe3dfe266b803abbac7e06e5aa531b63612e5
Eve <evenlybigman@gmail.com>
2026-04-06 20:23:00 +0900

    6주차 온라인 수업

---
+++ week06_circle/Circle.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int radius);
+	double getArea();
+	int getRadius();
+	void setRadius(int radius);
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 " << radius << endl;
+}
+
+Circle::Circle(int radius) {
+	this->radius = radius;
+	cout << "생성자 실행 " << radius << endl;
+}
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+int Circle::getRadius() {
+	return radius;
+}
+
+void Circle::setRadius(int radius) {
+	this->radius = radius;
+}
+
+void increase(Circle* c) {
+	int r = c->getRadius();
+	r++;
+	c->setRadius(r);
+}
+
+int main() {
+	Circle waffle(30);
+	
+	increase(&waffle);
+	cout << waffle.getRadius() << endl;
+}
