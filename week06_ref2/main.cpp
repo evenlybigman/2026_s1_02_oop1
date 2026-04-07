7e86b09095362d510919922bd96f207f547fe05c
Eve <evenlybigman@gmail.com>
2026-04-07 17:29:05 +0900

    참조 리턴 활용 실습

---
+++ week06_ref2/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int radius) { this->radius = radius; }
+	Circle& plus(int n);
+	int getRadius() { return radius; }
+};
+
+Circle& Circle::plus(int n) {
+	radius += n;
+	return *this;
+}
+
+int main() {
+	Circle a(5);
+	a.plus(1).plus(5).plus(200);
+	cout << a.getRadius() << endl;
+
+	string x("hello");
+	x.append(" Kitae").append("!");
+	cout << x;
+	return 0;
+}
