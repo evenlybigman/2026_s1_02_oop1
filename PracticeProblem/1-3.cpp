7c2333aa21771d6ffd0613daf6a63e72b9b73dbd
Eve <evenlybigman@gmail.com>
2026-04-14 18:05:05 +0900

    1-3 추가

---
+++ PracticeProblem/1-3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int x, result;
+	cout << "정수를 입력하세요>>";
+	cin >> x;
+	result = x / 10;
+	result %= 10;
+	cout << "245의 10자리 수는 " << result << "입니다.";
+}
